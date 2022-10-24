46f0938ed588da93836f23384d099159dee6d981
jrivera604 <joshrivera91@hotmail.com>
2022-10-24 00:12:43 +0000

    dice roller revised

--- dice-roller.js
+++ dice-roller.js
@@ -3,12 +3,11 @@ let input = (args.slice(2));
 let result = "";
 const rollDice = function(roll) {
   for (let i = 1; i <= roll; i++) {
-    const list = roll[i];
-    if (i  < roll){
-    result += Math.floor(Math.random() * 6 + 1) + ", ";
-    }
-    else {
-      result += Math.floor(Math.random() * 6 + 1) + "";  
+    
+    if (i  < roll) {
+      result += Math.floor(Math.random() * 6 + 1) + ", ";
+    } else {
+      result += Math.floor(Math.random() * 6 + 1) + "";
     }
   }
 
