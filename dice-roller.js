f0b85f9bd5964a29df3629e1aebef7098d363f86
jrivera604 <joshrivera91@hotmail.com>
2022-10-24 00:10:02 +0000

    dice roller

---
+++ dice-roller.js
@@ -0,0 +1,17 @@
+let args = process.argv;
+let input = (args.slice(2));
+let result = "";
+const rollDice = function(roll) {
+  for (let i = 1; i <= roll; i++) {
+    const list = roll[i];
+    if (i  < roll){
+    result += Math.floor(Math.random() * 6 + 1) + ", ";
+    }
+    else {
+      result += Math.floor(Math.random() * 6 + 1) + "";  
+    }
+  }
+
+  return result;
+};
+console.log(`Rolled ${input} dice: `, rollDice(input));
